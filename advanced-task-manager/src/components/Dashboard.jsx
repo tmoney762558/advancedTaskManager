d6456a4dc1b400aa00d48bddfe5f5fc523b60762
Ty <131617288+tmoney762558@users.noreply.github.com>
2024-11-21 12:32:28 -0600

    Added task color selector

--- advanced-task-manager/src/components/Dashboard.jsx
+++ advanced-task-manager/src/components/Dashboard.jsx
@@ -8,31 +8,40 @@ const Dashboard = () => {
   const [taskGroups, setTaskGroups] = useState([]);
   const [currentGroup, setCurrentGroup] = useState(null);
   const [currentWindow, setCurrentWindow] = useState("dashboard");
+  const priorityValues = ["Low", "Medium", "High", "Urgent"];
 
   const groupInput = useRef();
+  const colorInput = useRef();
   const taskInput = useRef();
 
+  // Main Objective 11/20/24: Implement selecting colors and urgency for tasks as well as fully implement the side nav. Also add local storage.
+  // Side Objectives 11/20/24:
+
   return (
-    <div className="w-full h-full relative lg:px-siteXLg px-siteXSm lg:pl-[4.7rem] pl-[4rem]">
+    <div className="w-full h-full relative lg:px-siteXLg px-siteXSm lg:pl-[4.7rem] pl-[4.7rem]">
       <SideNav></SideNav>
       {currentWindow === "dashboard" ? (
         <div className="flex flex-col justify-center items-center w-full">
           <h1 className="mt-5 text-2xl font-bold">Dashboard</h1>
-          <div className="flex justify-center items-center relative mt-7">
+          <div className="flex justify-center items-center w-full mt-7">
             <input
-              className="min-w-[25rem] py-2 pl-5 bg-neutral-200 rounded-full outline-none"
+              className="w-full max-w-[35rem] py-2 pl-5 pr-[2.5rem] bg-neutral-200 rounded-full outline-none"
               type="text"
               placeholder="Create a New Group"
               ref={groupInput}
             ></input>
             <button
-              className="w-10 aspect-square absolute right-0 bg-neutral-300 rounded-full"
+              className="w-10 aspect-square ml-[-2rem] bg-neutral-300 rounded-full"
               onClick={() => {
                 const input = groupInput.current.value;
+                if (input === "") {
+                  alert("Please enter a valid value.");
+                  return;
+                }
                 groupInput.current.value = "";
                 setTaskGroups((prevGroups) => [
                   ...prevGroups,
-                  { name: input, color: "red", tasks: [] },
+                  { name: input, color: "black", tasks: [] },
                 ]);
               }}
             >
@@ -49,7 +58,7 @@ const Dashboard = () => {
                 key={index}
               >
                 <p
-                  className="text-lg font-mono"
+                  className="flex-1 overflow-hidden whitespace-nowrap text-ellipsis text-lg font-mono"
                   onClick={() => {
                     setCurrentGroup(index);
                     setCurrentWindow("taskView");
@@ -58,11 +67,15 @@ const Dashboard = () => {
                 >
                   {group.name}
                 </p>
-                <div className="flex gap-5">
-                  <div
-                    className="w-5 aspect-square"
-                    style={{ backgroundColor: group.color }}
-                  ></div>
+                <div className="flex flex-1 justify-end items-center gap-5">
+                  <input
+                    className="w-[1.5rem] h-[1.7rem] bg-transparent"
+                    type="color"
+                    defaultValue={group.color}
+                    onChange={(e) => {
+                      group.color = e.target.value;
+                    }}
+                  ></input>
                   <IoClose
                     className="cursor-pointer"
                     onClick={() => {
@@ -79,9 +92,9 @@ const Dashboard = () => {
       ) : null}
       {currentWindow === "taskView" ? (
         <div className="flex flex-col justify-center items-center w-full">
-          <div className="flex justify-end relative w-full py-5">
+          <div className="flex justify-end w-full py-5">
             <IoClose
-              className="absolute cursor-pointer"
+              className="cursor-pointer"
               fontSize={"1.5rem"}
               onClick={() => {
                 setCurrentWindow("dashboard");
@@ -91,28 +104,28 @@ const Dashboard = () => {
           <h1 className="mt-5 text-2xl font-bold">
             {taskGroups[currentGroup].name}
           </h1>
-          <div className="flex justify-center items-center relative mt-7">
+          <div className="flex justify-center items-center w-full mt-7">
             <input
-              className="min-w-[25rem] py-2 pl-5 bg-neutral-200 rounded-full outline-none"
+              className="w-full max-w-[35rem] py-2 pl-5 pr-[2.5rem] bg-neutral-200 rounded-full outline-none"
               type="text"
               placeholder="Create a New Task"
               ref={taskInput}
             ></input>
-
             <button
-              className="w-10 aspect-square absolute right-0 bg-neutral-300 rounded-full"
+              className="w-10 aspect-square ml-[-2rem] bg-neutral-300 rounded-full"
               onClick={() => {
                 const input = taskInput.current.value;
+                if (input === "") {
+                  alert("Please enter a valid value.");
+                  return;
+                }
                 taskInput.current.value = "";
                 setTaskGroups((prevTaskGroups) =>
                   prevTaskGroups.map((group, index) =>
                     index === currentGroup
                       ? {
                           ...group,
-                          tasks: [
-                            ...group.tasks,
-                            { name: input, priority: "Urgent" },
-                          ],
+                          tasks: [...group.tasks, { name: input, priority: 0 }],
                         }
                       : group
                   )
@@ -131,9 +144,19 @@ const Dashboard = () => {
             } cursor-pointer`}
                 key={index}
               >
-                <p className="font-mono">{task.name}</p>
-                <div className="flex items-center gap-5">
-                  <p className="font-mono">Priority: {task.priority}</p>
+                <p className="flex-1 w-full overflow-hidden whitespace-nowrap text-ellipsis font-mono">
+                  {task.name}
+                </p>
+                <div className="flex flex-1 justify-end items-center gap-5">
+                  <p className="font-mono" onClick={() => {
+                    task.priority++;
+                    if (task.priority === 5) {
+                      task.prority === 0;
+                    }
+                    setTaskGroups(taskGroups);
+                  }}>
+                    Priority: {priorityValues[task.priority]}
+                  </p>
                   <IoClose
                     className="cursor-pointer"
                     fontSize={"1.3rem"}
